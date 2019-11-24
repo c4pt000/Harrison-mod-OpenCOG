a032f0f9fa52ec42ef3dff877310841f462c63dc
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-11-24 08:37:33 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,4 @@ docker build -t harrisonf . --no-cache
 docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/snd c4pt/harrison
 
 
-[root@localhost HarrisonF-TTS-mod-hanbot]# cat deploy-docker-and-build.sh 
-# run as root for pulseaudio, for X11 forwarding via docker check selinux settings or !!lastly dangeously in work environemnts other security !unsafe -> setenforce 0 
-
-xhost SI:localuser:root
-mkdir -p /home/developer
-
-docker build -t harrisonf . --no-cache
-docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/snd c4pt/harrisontts
+[
