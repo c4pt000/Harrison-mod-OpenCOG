b4cd0b2540319ea1ece9e1070d501cea7eb83335
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-11-24 09:07:31 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ xhost SI:localuser:root
 mkdir -p /home/developer
 
 docker build -t harrisonf . --no-cache
-docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/snd c4pt/harrison
+docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/snd c4pt/harrison-startcmd
 
 
 [
