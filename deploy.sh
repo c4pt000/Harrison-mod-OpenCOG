5076fbeb144dc0049b80f4fc44d6ea5fb41fa121
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-02-14 01:38:05 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ xhost SI:localuser:root
 #mkdir -p /home/developer
 
 # docker build -t harrisonf . --no-cache
-docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/snd c4pt/harrison-current
+docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -e AUDIO_DRIVER=pa,server=unix:/tmp/pulseaudio.socket \
+-v "/run/user/$(id -u)/pulse/native:/tmp/pulseaudio.socket" --device /dev/snd c4pt/harrison-current
 
 
